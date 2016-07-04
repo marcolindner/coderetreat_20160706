0dc6ff04ed900946d72fb73162ca0253c4198b8b
Marco Lindner <marco.lindner@gmx.org>
2016-07-04 09:46:59 +0200

    Adding Coverage Reporter

--- karma.conf.js
+++ karma.conf.js
@@ -19,6 +19,13 @@ module.exports = function(config) {
       'js/*.spec.js'
     ],
 
+    // Plugins
+
+    plugins: [
+      'karma-coverage',
+      'karma-jasmine',
+      'karma-phantomjs-launcher'
+    ],
 
     // list of files to exclude
     exclude: [
@@ -28,14 +35,21 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'js/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
+    // Coverage reporter
+
+    coverageReporter: {
+      type: 'text' ,
+      dir: 'test-results/coverage/'
+    },
 
     // web server port
     port: 9876,
